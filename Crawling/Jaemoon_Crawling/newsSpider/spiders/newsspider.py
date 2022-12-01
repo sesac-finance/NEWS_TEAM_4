5fe7499599de98d0f85df48c8d70805629066755
OliverJoo <jjaemoon@nate.com>
2022-12-01 11:27:23 +0900

    sub category add

--- Crawling/Jaemoon_Crawling/newsSpider/spiders/newsspider.py
+++ Crawling/Jaemoon_Crawling/newsSpider/spiders/newsspider.py
@@ -17,25 +17,32 @@ class NewsspiderSpider(scrapy.Spider):
     max_page = 0
 
     def start_requests(self):
-        date = datetime.date.today()
-        target_date = date - datetime.timedelta(days=1)  # 크롤링 기간
         main_categories = ['foreign']  # 'digital'  # 국제뉴스 : foreign , IT : digital
-        sub_categories = ['america', 'asia', 'europe', 'africa', 'others', 'englishnews', 'topic', 'japan', 'china']
-        page_number = 1
+        sub_categories = ['asia', 'america', 'europe', 'africa', 'others', 'englishnews', 'topic', 'japan', 'china']
+        sub_categories_kor = ['아시아/대양주', '미국/아메리카', '유럽', '중동/아프리카', '국제일반', '영어뉴스', '해외화제', '일본', '중국']
+        page_number = 0
 
         for main_category in main_categories:
-            while target_date != date:  # 현재로부터 얼마나 과거의 날들을 크롤링할지
-                for sub_category in sub_categories:
+            for sub_category in sub_categories:
+                date = datetime.date.today()
+                target_date = date - datetime.timedelta(days=1)  # 크롤링 기간
+                while target_date != date:  # 현재로부터 얼마나 과거의 날들을 크롤링할지
+
                     try:
+                        page_number += 1
+
                         date_str = date.strftime('%Y%m%d')
 
-                        result = requests.get(
-                            f'{base_url}{main_category}/{sub_category}?page={page_number}&regDate={date_str}')
+                        result = requests.get(f'{base_url}{main_category}/{sub_category}?page={page_number}&regDate={date_str}')
                         source = BeautifulSoup(result.text, 'html.parser')
                         pages = source.find('span', class_='inner_paging').text.split()
 
-                        response = scrapy.Request(f'{base_url}{main_category}?page={page_number}&regDate={date_str}',
-                                                  self.parse_url, meta={'sub_category': sub_category})
+                        print(f'pages for test:{base_url}{main_category}/{sub_category}?page={page_number}&regDate={date_str}')
+
+                        response = scrapy.Request(f'{base_url}{main_category}/{sub_category}?page={page_number}&regDate={date_str}',
+                                                  self.parse_url,
+                                                  meta={'sub_category': sub_categories_kor[
+                                                      sub_categories.index(sub_category)]})
                         yield response
 
                         pages_size = len(pages)
@@ -43,14 +50,11 @@ class NewsspiderSpider(scrapy.Spider):
                             number = re.sub(r'[^0-9]', '', pages[pages_size - 1])
                             print("for test : ", '현재' in pages[pages_size - 2].strip(), int(page_number) == int(number),
                                   page_number, number, pages[pages_size - 2], pages[pages_size - 1])
-                            if '현재' in pages[pages_size - 2].strip() and int(
-                                    page_number) >= number and sub_categories.index(sub_category) == 8:
+                            if '현재' in pages[pages_size - 2].strip() and int(page_number) >= int(number):
                                 date -= datetime.timedelta(days=1)
                                 page_number = 1
                                 # print('날짜 변경!')
-                                pass
 
-                        page_number += 1
 
                     except Exception as e:
                         print("Exception : ", e)
@@ -72,7 +76,7 @@ class NewsspiderSpider(scrapy.Spider):
         scrawl_info = {
             # 'ID': 'id',
             'DomainID': '0',  # daum : 0 / naver : 1
-            'MainCategory': 'main_categories',
+            'MainCategory': 'foreign',
             'SubCategory': response.meta['sub_category'],
             'WritedAt': 'writed_at',
             'Title': title,
